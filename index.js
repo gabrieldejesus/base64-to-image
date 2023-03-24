90dea823f71ebf5480c6786edd19f83a98962082
Gabriel de Jesus <devgabrieldejesus@gmail.com>
2023-03-24 19:34:34 -0300

    feat: remove image.png

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 
 function base64ToImage(base64String, path) {
   const base64Data = base64String.replace(/^data:image\/\w+;base64,/, '');
+  
   const buffer = new Buffer.from(base64Data, 'base64');
+  
   fs.writeFile(path, buffer, (error) => {
     if (error) throw error;
     console.log('The file has been saved!');
